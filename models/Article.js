50d0aa9f1e6313dd44c86deb80c13d106b7f3396
Sthefano Carvalho <sthefano@masteradin.com>
2021-03-03 21:11:09 -0300

    config: mongodb models adjusted

--- models/Article.js
+++ models/Article.js
@@ -11,7 +11,10 @@ var ArticleSchema = new mongoose.Schema({
   favoritesCount: {type: Number, default: 0},
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
   tagList: [{ type: String }],
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  originalContent: String,
+  link: String,
+  infographicCode: String
 }, {timestamps: true});
 
 ArticleSchema.plugin(uniqueValidator, {message: 'is already taken'});
@@ -49,7 +52,9 @@ ArticleSchema.methods.toJSONFor = function(user){
     tagList: this.tagList,
     favorited: user ? user.isFavorite(this._id) : false,
     favoritesCount: this.favoritesCount,
-    author: this.author.toProfileJSONFor(user)
+    author: this.author.toProfileJSONFor(user),
+    link: this.link,
+    infographicCode: this.infographicCode
   };
 };
 
